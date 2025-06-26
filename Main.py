6881796cb563b4389bd262327a78ad52a3e3fe01
J.Graf <jannik.graf@stud.uni-due.de>
2025-06-26 11:07:40 +0200

    Jax Forward Gradient hinzugefügt. aber noch errors

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 import linearRegressionJax as lR_Jax
 import linearRegressionTorch as lR_Torch
+import linearRegressionForwardGradient as lR_JaxFG
 
 if __name__ == '__main__':
     a_jax, b_jax = lR_Jax.startTrain()
     a_torch, b_torch = lR_Torch.startTrain()
+    a_jax_fg, b_jax_fg = lR_JaxFG.startTrain()
 
-    print(f'\nJAX\t\t y = {a_jax:.6f} * X + {b_jax:.6f}')
+    print(f'\nJAX - backprop\t y = {a_jax:.6f} * X + {b_jax:.6f}')
     print(f'Torch\t y = {a_torch:.6f} * X + {b_torch:.6f}\n')
+    print(f'JAX - FG\t y = {a_jax_fg:.6f} * X + {b_jax_fg:.6f}\n')
--- linearRegressionForwardGradient.py
+++ linearRegressionForwardGradient.py
@@ -1,7 +1,9 @@
+import jax
 import jax.numpy as jnp
-from jax import grad, jit
+from jax import random
 import yaml
 import matplotlib.pyplot as plt
+from sympy.printing.theanocode import theano
 
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
@@ -9,56 +11,54 @@ with open("config.yaml", "r") as file:
 learningRate = config["learningRate"]
 iterations = config["iterations"]
 patience = config["patience"]
-precision = config["precision"]
+precision = 1e-5
 value_a = config["a"]
 value_b = config["b"]
 data_x = config["x"]
 data_y = config["y"]
 
-X_jax = jnp.array(data_x)
+X_jax = jnp.stack(data_x, jnp.ones_like(data_x),axis=1)
 Y_jax = jnp.array(data_y)
 
-def linearFkt(a,b,x):
-    return a * x + b
-
-def calculationMSE(a, b):
-    Y_pred = linearFkt(a,b,X_jax)
-    return jnp.mean((Y_pred - Y_jax) ** 2)
+def calculationMSE(theta,X,y):
+    Y_pred = X @ theta
+    return jnp.mean((Y_pred - y) ** 2)
 
 def startTrain(plot=False):
-    a = jnp.array(value_a, dtype=float)
-    b = jnp.array(value_b, dtype=float)
     print(f'\nStarting training lReg with JAX...\n'
-          f'\tYour Funktion: y = {a.item():.6f} * X + {b.item():.6f}\n'
+          f'\tYour Funktion: y = {value_a} * X + {value_b}\n'
           f'\tIterations: {iterations} \n '
           f'\tlearning rate: {learningRate} \n '
           f'\tprecision: {precision}')
-
     loss_list = []
-    previous_digit = 0
+    best_loss = jnp.inf
     counter = 0
-    compute_gradients = jit(grad(calculationMSE, argnums=(0,1)))
+    theta = jnp.array([value_a,value_b], dtype=float)
+    key = random.PRNGKey(42)
+
     for i in range(iterations):
-        loss = calculationMSE(a, b)
+        key, subkey = random.split(key)
+
+        v = random.normal(key, shape=theta.shape)
+        f_val, directional_derivative = jax.jvp(lambda th: calculationMSE(th,X_jax,Y_jax),(theta,),(v,))
+        g_theta = directional_derivative * v
+        theat_new = theta - learningRate * g_theta
+
+        loss = f_val
         loss_list.append(loss.item())
-        #print(f'Iteration: {i + 1} \n Loss: {loss.item():.4f} \n a: {a.item():.6f}\n b: {b.item():.6f}')
-        # Update Parameters
-        thet
-        grad_a, grad_b = compute_gradients(a,b)
-        a = a - learningRate * grad_a
-        b = b - learningRate * grad_b
-        digit = getDecimalDigit(a, precision)
         # print(f'Your New Funktion: y = {a.item():.6f} * X + {b.item():.6f}\n')
 
-        # Meine "Converged?" Abfrage
-        if digit == previous_digit:
+        # Early Stopping: Converged to?
+        if jnp.abs(loss-best_loss) < precision:
             counter += 1
+            if counter >= patience:
+                print(f"\tStopping early at Iteration {i + 1}-Change < {precision}\n")
+                break
         else:
             counter = 0
-        previous_digit = digit
-        if counter >= patience:
-            print(f"\tStopping early at Iteration {i + 1} as the {precision}-th decimal hasn´t changed")
-            break
+            best_loss = loss
+        a_learned, b_learned = theat_new
+        return a_learned, b_learned
 
     # Plotting the loss
     if plot:
