d8bfcd285e01fcb3dd0681e18721ea90b0b7883c
J.Graf <jannik.graf@stud.uni-due.de>
2025-08-26 12:48:48 +0200

    problems with the MNIST Dataset

--- NN-Training/JAX_NNandDataLoading.py
+++ NN-Training/JAX_NNandDataLoading.py
@@ -4,10 +4,12 @@ import numpy as np
 from jax.tree_util import tree_map
 from torch.utils.data import DataLoader, default_collate
 from torchvision.datasets import MNIST
+import time
 
 from jax import grad, jit, vmap
 from jax import random
 from jax.scipy.special import logsumexp
+
 
 # We need a fucntion to intilize the weights and biases for a dense neural network layer
 
@@ -26,7 +28,7 @@ layer_sizes = [784, 512, 512, 10]
 step_size = 0.01
 num_epochs = 8
 batch_size = 128
-n_targets = 10
+n_targets = 10 # handschriftliche Ziffern 0-9
 params = init_network_params(layer_sizes, random.key(0))
 
 def relu(x):
@@ -111,3 +113,24 @@ mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=flatten_and_cast)
 
 # create pytorch data loader with custom collate function
 training_generator = DataLoader(mnist_dataset, batch_size=batch_size, collate_fn=numpy_collate)
+
+# Get the full train dataset (for checking accuracy while training)
+train_images = np.array(mnist_dataset.data).reshape(len(mnist_dataset.data), -1)
+train_labels = one_hot(np.array(mnist_dataset.targets), n_targets)
+# Get full test dataset
+mnist_dataset_test = MNIST('/tmp/mnist/', download=True, train=False)
+test_images = jnp.array(mnist_dataset_test.data.numpy().reshape(len(mnist_dataset_test.data), -1), dtype=jnp.float32)
+test_labels = one_hot(np.array(mnist_dataset_test.targets), n_targets)
+
+for epoch in range(num_epochs):
+  start_time = time.time()
+  for x, y in training_generator:
+    y = one_hot(y, n_targets)
+    params = update(params, x, y)
+  epoch_time = time.time() - start_time
+
+  train_acc = accuracy(params, train_images, train_labels)
+  test_acc = accuracy(params, test_images, test_labels)
+  print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+  print("Training set accuracy {}".format(train_acc))
+  print("Test set accuracy {}".format(test_acc))
