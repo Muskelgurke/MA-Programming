cb75661fb2987fde9b762cd8cbce2acb1ca96c62
Jannik Graf <jannik.graf@gmx.de>
2025-10-14 16:45:44 +0200

    add models for cifar10 100 and mnist fashionmnist
    multiple config runs kann train now over multiple datasets

--- torch_nn_training/model/model.py
+++ torch_nn_training/model/model.py
@@ -6,24 +6,57 @@ def get_model(config: Config) -> nn.Module:
     match config.dataset_name.lower():
         case "mnist" | "fashionmnist":
             input_size = 28 * 28
-            output_size = 10  # number of classes
-
+            output_size = 10
             match config.model_type:
                 case "fc256":
                     hidden_size = 256
-                    return NN_fc_for_MNIST(input_size=input_size,hidden_size=hidden_size, output_size=output_size)
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
                 case "fc1024":
                     hidden_size = 1024
-                    return NN_fc_for_MNIST(input_size=input_size,hidden_size=hidden_size, output_size=output_size)
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
                 case "fc4096":
                     hidden_size = 4096
-                    return NN_fc_for_MNIST(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
-
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
                 case "cnn":
                     return CNN_for_MNIST()
                 case _:
                     raise ValueError(f"Unknown model type: {config.model_type} for that Dataset")
-        #case "cifar10":
+
+        case "cifar10":
+            input_size = 32 * 32 * 3
+            output_size = 10
+            match config.model_type:
+                case "fc256":
+                    hidden_size = 256
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
+                case "fc1024":
+                    hidden_size = 1024
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
+                case "fc4096":
+                    hidden_size = 4096
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
+                case "cnn":
+                    return CNN_for_CIFAR(output_size=output_size)
+                case _:
+                    raise ValueError(f"Fully connected models not implemented for CIFAR-10")
+
+        case "cifar100":
+            input_size = 32 * 32 * 3
+            output_size = 100
+            match config.model_type:
+                case "fc256":
+                    hidden_size = 256
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
+                case "fc1024":
+                    hidden_size = 1024
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
+                case "fc4096":
+                    hidden_size = 4096
+                    return NN_fc(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
+                case "cnn":
+                    return CNN_for_CIFAR(output_size=output_size)
+                case _:
+                    raise ValueError(f"Fully connected models not implemented for CIFAR-10")
 
         case "demo_linear_regression":
             # Add implementation for linear regression model
@@ -39,9 +72,9 @@ def get_model(config: Config) -> nn.Module:
 
 
 
-class NN_fc_for_MNIST(nn.Module):
+class NN_fc(nn.Module):
     def __init__(self, input_size: int, hidden_size:int, output_size: int):
-        super(NN_fc_for_MNIST, self).__init__()
+        super(NN_fc, self).__init__()
 
         # Original NN structure
         self.flatten = nn.Flatten()
@@ -77,9 +110,45 @@ class linear_regression_model(nn.Module):
         """Return the weights (a) and bias (b) of the linear layer"""
         return self.linear.weight.data, self.linear.bias.data
 
+class CNN_for_CIFAR(nn.Module):
+    """Convolutional Neural Network for Cifar10/100 dataset
+    aus dem Paper BAYDIN et al. 2022
+    input 3x32x32 (3 Kanäle für RGB)
+    """
+    def __init__(self, output_size= 0):
+        super(CNN_for_CIFAR, self).__init__()
+
+        self.features = nn.Sequential(
+
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Second conv block
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), # Output: 64 x 8 x 8
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(64 * 8 * 8, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, output_size) # Output: 10 Klassen
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
 
 class CNN_for_MNIST(nn.Module):
-    """Convolutional Neural Network for MNIST dataset
+    """Convolutional Neural Network for MNIST/FashionMnist dataset
     aus dem Paper BAYDIN et al. 2022
     """
     def __init__(self):
