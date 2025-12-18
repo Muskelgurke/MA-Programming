9408db20bbf4cd3a15cbd87235888e12e77df7c5
Jannik Graf <jannik.graf@gmx.de>
2025-12-18 10:57:52 +0100

    add model.eval()

--- methods/fg_trainer.py
+++ methods/fg_trainer.py
@@ -23,7 +23,7 @@ class ForwardGradientTrainer(BaseTrainer):
             for batch_idx, (inputs, targets) in enumerate(pbar):
                 inputs, targets = inputs.to(self.device), targets.to(self.device)
                 self.optimizer.zero_grad()
-                self.model.train()
+                self.model.eval()
                 # Warmup forward pass to ensure buffers are initialized
                 self.model(inputs)
 
